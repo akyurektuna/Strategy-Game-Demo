92f6932af122489faacfab21b117d487c6fab9eb
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2020-03-18 18:50:56 +0300

    GameManager and LevelManager scripts are created
    Manager scripts which are to take care of running time functionalities and start functionalities are created. Singleton class is created.
    The scripts that supposed to handle panel buttons(production menu) should be revisited.

---
+++ Strategy Game Demo/Assets/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    [SerializeField] 
+    private GameObject tileObject;
+    public float TileSize{
+        get{return tileObject.GetComponent<SpriteRenderer>().sprite.bounds.size.x;}
+    }
+    // Start is called before the first frame update
+    void Start(){ 
+        CreateLevel();
+        }
+
+    //making background tile for the camera view so the bounds are edges of the camera view
+    // using just one type of tile  
+    private void CreateLevel(){
+        //worldStart is the starting point for the background tiles which is top left corner of the camera
+       Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0,Screen.height));
+        Camera cam = Camera.main;
+        //taking height and width to calculate how many tiles to put on x and y axes
+        float height = 2f*cam.orthographicSize;
+        float width = height*cam.aspect;
+        for( int y=0;y<height/TileSize;y++){
+            for(int x=0; x<width/TileSize; x++){
+                GameObject newTile = Instantiate(tileObject);
+                 newTile.AddComponent<TileScript>();
+                newTile.transform.position = new Vector3(worldStart.x+TileSize*x,worldStart.y- TileSize*y,0);
+            }
+        }
+    
+    }
+}
---
+++ Strategy Game Demo/Assets/TileScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver() {
+        Debug.Log("helov");
+        if(Input.GetMouseButton(0)){
+            PlaceTower();
+        }
+        
+    }
+
+    private void PlaceTower(){
+
+    }
+}
