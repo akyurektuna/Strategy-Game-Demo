b93edb6a1134aaa882cb4225927b64dd06cd83a8
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2020-03-20 16:35:18 +0300

    SelectionManager updates
    information panel shows clicked structure's information and producible units(if that structure can produce units).

--- Strategy Game Demo/Assets/SelectionManager.cs
+++ Strategy Game Demo/Assets/SelectionManager.cs
@@ -6,16 +6,25 @@ using UnityEngine.UI;
 public class SelectionManager : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
+    [SerializeField] private GameObject childPanel;
     /*
     In the map, a background tile and structure tile can be at the same position on top of each other
         so to choose the desired one Selectable Tag is initialized and used.
     */
     [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Sprite buttonImg;
 
+    private  GameObject panelTextObj;
+    private Text text;
+    
+private void Start() {
+    panelTextObj = new GameObject();  
+    text = panelTextObj.AddComponent<Text>();  
+}
     void Update()
     {
         Debug.Log("update?");
-        
+       
         if(Input.GetMouseButtonDown(0)){
 
             //casting a ray to click on a structure to update information panel
@@ -38,20 +47,39 @@ public class SelectionManager : MonoBehaviour
     }
 
 
-    void updateInfoPanel(GameObject gameObj){
-        GameObject newText = new GameObject();
+   public void updateInfoPanel(GameObject gameObj){
         Barracks b = new Barracks();
-       //need to have the structure (structure that the ray hit and has been created before) and use that structures info method.
-
-        Text text = newText.AddComponent<Text>();
+ 
         text.text = b.structureInfo();
  
         Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
         text.font = ArialFont;
         text.material = ArialFont.material;
 
-        newText.transform.SetParent(panel.transform);
-        newText.transform.position = newText.transform.parent.position;
+        panelTextObj.transform.SetParent(panel.transform);
+        panelTextObj.transform.position = panelTextObj.transform.parent.position;
+
+        CreateButton(childPanel.transform,childPanel.transform.position);        
     }
+
+
+
+    public void CreateButton(Transform panel ,Vector3 position){
+        GameObject button = new GameObject();
+        button.transform.parent = panel;
+        button.AddComponent<RectTransform>();
+        button.AddComponent<Button>();
+        button.transform.position = position;
+        button.AddComponent<Image>();
+        button.GetComponent<Image>().sprite = buttonImg;
+    //button.GetComponent<RectTransform>().SetSize(size);
+    //last parameter UnityEngine.Events.UnityAction method
+    //button.GetComponent<Button>().onClick.AddListener(method);
+}
+
+public void methodButton(){
+
+}
+
 }
 
